c7b97ce2fb738a12fc906927346ddd9088127a37
rmcm <rmcm@g1c10.cin.ufpe.br>
2025-06-12 18:35:57 -0300

    modularizated case_generator

--- main.py
+++ main.py
@@ -4,21 +4,12 @@ from data_set import data_set
 
 jorge: NeuralNetwork = NeuralNetwork([784, 16, 16, 10])
 
-""" generate 28*28 grid on console
-print('\"\"\"')
-for i in range (28):
-	for j in range(28):
-		x = str(randint(0,99))
-		x = " "*(2-len(x)) + x 
-		print(x, end=", ")
-	print()
-print('\"\"\"')
-"""
 
 case = data_set[0]
 
 
 jorge.process(case[0])
+
 jorge.backpropagate(case[1])
 
 jorge.print_output()
