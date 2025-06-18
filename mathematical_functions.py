7c27b9618161ffdd50ab9bb55a2f74df7fc9bde9
rmcm <rmcm@cin.ufpe.br>
2025-06-18 17:07:02 -0300

    modularization of general mathematical functions

--- mathematical_functions.py
+++ mathematical_functions.py
@@ -2,3 +2,23 @@
     auxiliary mathematical functions
     used in neural network implementation
 """
+
+from data import Vector
+from math import e
+
+def sigmoid(z: Vector) -> Vector:
+	out: Vector
+	out = (1 / (1 + (e ** (-z))))
+	return out
+
+
+def sigmoid_derivative(z: Vector) -> Vector:
+	out: Vector = sigmoid(z) * (1 - sigmoid(z))
+	return out
+
+
+def quadratic_loss(target: Vector, prediction: Vector) -> float:
+	cost: float = 0
+	for i_activation in range(len(target)):
+		cost += (prediction[i_activation] - target[i_activation])
+	return cost
