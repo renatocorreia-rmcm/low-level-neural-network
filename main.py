507791fcf47c5f35ca9507ad1c3567121bba211c
Renato Correia <rmcm@cin.ufpe.br>
2025-06-18 10:20:11 -0300

    encapsulation idea

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from NeuralNetwork import NeuralNetwork
 from data import Vector
 
+"""
+    my data types can be entirely "encapsulate"
+    do all conversions inside of class, so it can recieve and output lists instead of user defined vectors
+"""
+
 jorge: NeuralNetwork = NeuralNetwork([3, 3, 2])
 
 
