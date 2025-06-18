65cc0c4866da270ec9b238207fa72bb4357f67bb
renatocorreia12144-rmcm <rmcm@cin.ufpe.br>
2025-06-17 21:24:46 -0300

    nova convenção de indexação de parametros; remoção de sketch antigo de backpropagation

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -18,11 +18,26 @@ biases(i) são biases que entram na layer(i)
 # BIASES[i]   PERTENCEM A LAYER[i] -> geram layer[i]    # i  ]0, n-1]
 """
 
+""" Nova	Conveção da indexação dos parâmetros da rede neural
+
+pesos(i) são conexões que saem de layer(i-1) para layer(i)  
+	logo, weights[i] é usado com layer[i-1] para gerar layer[i]
+	primeiro elemento de self.weights: None
+
+biases(i) são biases que entram na layer(i)
+	primeiro elemente de self.biases: None 
+
+"""
+
+
 """ Convenção de nomeação de parâmetros referentes a camadas
 
-l0 e l1 não representam necessariamente self.layers[0] e self.layers[1],
+os parâmetros l0 e l1 não representam necessariamente self.layers[0] e self.layers[1],
 	mas sim self.layers[i] e self.layers[i+1] respectivamente,
-	para qualquer self.layers[i] que esteja em operaração
+	para qualquer i que tenha sido passado como argumento
+	
+	ou seja, l0 e l1 são usados para se referir a camadas adjacentes,
+	que não necessariamente são a 0 e a 1
 """
 
 from random import randint
@@ -33,13 +48,6 @@ from data import Matrix
 """
 	functions
 """
-
-
-def relu(z: Vector) -> Vector:
-	out: Vector = Vector(size=len(z))
-	for i, activation in enumerate(z):
-		out[i] = max(0.0, activation)
-	return out
 
 
 def sigmoid(z: Vector) -> Vector:
@@ -73,8 +81,6 @@ class NeuralNetwork:
 	# parameters
 	weights: list[Matrix]
 	biases: list[Vector]
-	# z
-	z: list[Vector]  # stored for computing partial derivative in backpropagation
 
 	"""
 		processing
@@ -84,18 +90,16 @@ class NeuralNetwork:
 		i_l0: int = i_l1 - 1
 		# calculate activation function parameters
 		l0: Vector = self.layers[i_l0]
-		weights_l0: Matrix = self.weights[i_l0]
+		weights_l1: Matrix = self.weights[i_l1]
 		biases_l1: Vector = self.biases[i_l1]
-		# z
-		z: Vector = weights_l0 * l0 + biases_l1
-		self.z[i_l1] = z  # store to access in backpropagation
+		z: Vector = weights_l1 * l0 + biases_l1
 		# apply activation function
-		self.layers[i_l1] = relu(z)
+		self.layers[i_l1] = sigmoid(z)
 
-	def process(self, feed: list[int]) -> Vector:
-		feed: Vector = Vector(feed)
+	def process(self, feature: list[int]) -> Vector:
+		feature: Vector = Vector(feature)
 		# load input
-		self.layers[0] = feed
+		self.layers[0] = feature
 		# execute propagation for each layer
 		for i_layer in range(1, self.n_layers):
 			self.activate(i_layer)
@@ -109,13 +113,10 @@ class NeuralNetwork:
 	# gradients - sum over all data points and take average later  # could it be permanently = {0} if all changes were done in a learn_batch call scope?
 	weights_gradients: list[Matrix]
 	biases_gradients: list[Vector]
-	# partial derivatives - reset after each data point
-	activations_partial_derivatives: list[Vector]
-	z_partial_derivatives: list[Vector]
 
 	"""operations"""
 
-	def reset_gradient(self):
+	def reset_gradient(self):  # do before start each learn of data_point
 		for i_layer in range(
 				self.n_layers):  # for each layer, reset his Vectors activations_gradient, biases_gradient and his Matrix weights_gradient
 			self.weights_gradients[i_layer].reset()
@@ -124,24 +125,12 @@ class NeuralNetwork:
 	def learn_batch(self, batch):
 
 		for data_point in batch:
-			"""cost"""
 			feature, target = data_point.split()
-			self.process(feature)  # calculate prediction
+			# forward go - calculate prediction
+			self.process(feature)
+			# reach end - calculate cost
 			cost: float = quadratic_loss(target, self.layers[-1])
-
-			"""vector: partial derivatives of cost in relation to each activation"""
-			# ∂(C0/aL)
-			aL: Vector = self.layers[-1]
-			vec: Vector = 2 * (aL - target)
-			"""vector: partial derivatives of activations in relation to each z"""
-			# ∂(aL/z)
-			zL: Vector = self.z[-1]
-			vec: Vector = sigmoid_derivative(zL)
-			"""list[matrix]: partial derivative of zL in relation to WL"""
-			# zL is a vec, while WL is a matrix. so we get a list, where each element is a matrix related to a element of ZL
-			# ∂(zL/WL)
-			al: Vector = self.layers[-2]  # a_l-1
-			vec: Vector
+			# backward go - calculate error signal for each layer - get gradient for each parameter
 
 	def backpropagate(self, cost: Vector, target: Vector):  # compute gradient
 		return
@@ -179,13 +168,6 @@ class NeuralNetwork:
 				print(f"layer_{i_layer} neuron_{i_neuron}: {bias}")
 			print()
 
-	def print_output(self) -> None:
-		print("\nOUTPUT:\n")
-		activations: list[str] = str(self.layers[-1]).split('\n')
-		for activation in activations:
-			print(activation)
-		print()
-
 	"""
 		constructor
 	"""
@@ -204,22 +186,21 @@ class NeuralNetwork:
 
 		"""initialize weights"""
 		# create and fill weights with random weights
-		self.weights = []
+		self.weights = [None]  # first layer has no weights
 		# create new matrixes wich represent (l_i weights)
-		for i_layer in range(self.n_layers - 1):  # last layer has no weights
-			weights: Matrix = Matrix()
-			size_l0 = self.layers_sizes[i_layer]  # size of output layer
-			size_l1 = self.layers_sizes[i_layer + 1]  # size of input layer
-			# create new lines wich represent (a_(l0, i_line) weights)
+		for i_layer in range(1, self.n_layers):
+			weights_l: Matrix = Matrix()
+			size_l0 = self.layers_sizes[i_layer - 1]  # size of output layer
+			size_l1 = self.layers_sizes[i_layer]  # size of input layer
+			# create new lines wich represent (a_(l1, i_line) weights)
 			for i_line in range(size_l1):
 				line: list[float] = []
 				# create new colls wich represent (a_(l1, i_line), a_(l0, i_col)) weight
 				for i_coll in range(size_l0):
 					weight: float = randint(-10, 10) / 10
 					line.append(weight)
-				weights.append(line)
-			self.weights.append(weights)
-		self.weights.append(None)  # last layer has no weights
+				weights_l.append(line)
+			self.weights.append(weights_l)
 
 		"""initialize biases"""
 		# create and fill biases with random biases
@@ -233,8 +214,3 @@ class NeuralNetwork:
 				bias: float = randint(-10, 10) / 10
 				biases.append(bias)
 			self.biases.append(biases)
-
-		"""initialize z"""
-		# fill z with void vectors to overwrite it after
-		for size in self.layers_sizes:
-			self.z.append(Vector(size=size))
