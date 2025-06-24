32c65f09cc7c238e4d1bce6691496fccdf302b14
renatocorreia-rmcm <rmcm@cin.ufpe.br>
2025-06-24 12:35:33 -0300

    docstrings, typing, new methods get_answer and get_accuracy, parametrization of method train_epoch

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -24,12 +24,12 @@ Convenção de nomeação de parâmetros referentes a camadas
 	que não necessariamente são a 0 e a 1
 """
 
-
 """
 	external dependencies
 """
 
-from random import randint
+from random import randint  # parameters initialization
+from random import shuffle  # data_set shuffle in each epoch
 
 from modules.data import Vector
 from modules.data import Matrix
@@ -49,10 +49,12 @@ from modules.file_functions import set_line
 
 class NeuralNetwork:
 	"""
-		fully connected neural network
-		of given size.
+		fully connected neural network of given size.
 		It name and parameters may be backuped automatically in a .txt,
 		well as loaded from the.txt back to the object.
+
+		Uses sigmoid as activation function
+		Uses quadratic loss as cost function
 	"""
 
 	# layers
@@ -69,7 +71,7 @@ class NeuralNetwork:
 
 	def activate(self, i_l1: int) -> None:
 		"""
-		:param i_l1: index of layer to compute activations
+		:param i_l1: index of layer to compute activations based in previous one (expected already calculated)
 		"""
 		i_l0: int = i_l1 - 1
 		# calculate activation function parameters
@@ -80,7 +82,7 @@ class NeuralNetwork:
 		# apply activation function
 		self.layers[i_l1] = sigmoid(z_l1)
 
-	def process(self, feature: list[float]) -> list[float]:
+	def process_feature(self, feature: list[float]) -> list[float]:
 		"""
 		:return: prediction
 		"""
@@ -92,6 +94,18 @@ class NeuralNetwork:
 			self.activate(i_layer)
 		return list(self.layers[-1])
 
+	def get_answer(self) -> int:
+		"""
+		:return: index of brighter output activation
+		"""
+		i_maior: int = 0
+		maior: float = self.layers[-1][i_maior]
+		for i in range(1, self.layers_sizes[-1]):
+			if self.layers[-1][i] > maior:
+				i_maior = i
+				maior = self.layers[-1][i]
+		return i_maior
+
 	"""
 		learning
 	"""
@@ -101,52 +115,79 @@ class NeuralNetwork:
 	biases_gradients: list[Vector]
 
 	learn_step: float
+
 	# operations
 
-	def learn_batch(self, batch: any) -> None:  # todo: keeping batch abstract until we actually have it
-		""" for data_point in batch:
-				process data_point 	- forward go
-				calculate cost 		- reach end
-				backpropagate 		- backward go
-			nn -= ▽C
+	def get_accuracy(self) -> float:
+		"""
+		process all trainning set
+		:return: accuracy between 0 and 1
+		"""
+		acertos: int = 0
+
+		for x_test, y_test in zip(self.x_test, self.y_test):
+			self.process_feature(x_test)
+			if self.get_answer() == y_test:
+				acertos += 1
+
+		return acertos/len(self.y_test)
+
+	def train_epoch(self, learn_step: float = 1) -> None:
+		"""
+			process and backpropagate all data_points in training_set
 		"""
 
-		# reset gradient
-		for i_layer in range(self.n_layers):
-			self.weights_gradients[i_layer].clean()
-			self.biases_gradients[i_layer].clean()
+		batch_size: int = 16
+		n_batches: int = int(len(self.y_train) / batch_size)
 
-		# somatory of each data_point gradient
-		for data_point in batch:
+		# random indexes order (to iterate over trainining set)
+		order: list[int] = list(range(len(self.x_train)))
+		shuffle(order)
+		i2: int = 0  # second order index (to iterate random indexes list)
 
-			feature: list[float]
-			target: Vector
-			feature = data_point[0]
-			target = Vector(data_point[1])
+		# somatory of each batch average gradient
+		for i_batch in range(n_batches):
 
-			# forward go - calculate prediction
-			prediction: Vector = Vector(self.process(feature))
-			# reach end - calculate cost
-			cost: float = quadratic_loss(prediction, target)
-			# print(f"COST: {cost}")
-			# backward go - calculate error signal for each layer then gradient for each parameter
-			gradients: tuple[list[Matrix], list[Vector]] = self.backpropagate(cost, target)
-			weights_gradients: list[Matrix] = gradients[0]
-			biases_gradients: list[Vector] = gradients[1]
+			# reset gradient
+			for i_layer in range(self.n_layers):
+				self.weights_gradients[i_layer].clean()
+				self.biases_gradients[i_layer].clean()
+
+			# somatory of each data_point gradient
+			for data_point in range(batch_size):
+
+				i1: int = order[i2]  # first order index (to iterate directly over training set)
+				i2 += 1
+
+				feature: list[float]
+				target: int
+				feature = self.x_train[i1]
+				target = self.y_train[i1]
+
+				# forward go - compute prediction
+				prediction: Vector = Vector(self.process_feature(feature))
+				# reach end - compute cost
+				cost: float = quadratic_loss(prediction, target)
+				# backward go - calculate error signal for each layer then gradient for each parameter
+				target_vec: Vector = Vector(size=self.layers_sizes[-1])
+				target_vec[target] = 1
+				gradients: tuple[list[Matrix], list[Vector]] = self.backpropagate(target_vec)
+				weights_gradients: list[Matrix] = gradients[0]
+				biases_gradients: list[Vector] = gradients[1]
+				for i_layer in range(1, self.n_layers):
+					self.weights_gradients[i_layer] += weights_gradients[i_layer]
+					self.biases_gradients[i_layer] += biases_gradients[i_layer]
+
+			# transform batch somatory into batch average
 			for i_layer in range(1, self.n_layers):
-				self.weights_gradients[i_layer] += weights_gradients[i_layer]
-				self.biases_gradients[i_layer] += biases_gradients[i_layer]
+				self.weights_gradients[i_layer] /= batch_size
+				self.biases_gradients[i_layer] /= batch_size
+			# subtract each partial derivative average from its corresponding parameter  # nn -= ▽C
+			for i_layer in range(1, self.n_layers):
+				self.weights[i_layer] -= self.weights_gradients[i_layer] * self.learn_step
+				self.biases[i_layer] -= self.biases_gradients[i_layer] * self.learn_step
 
-		# transform batch somatory into batch average
-		for i_layer in range(self.n_layers):
-			self.weights_gradients[i_layer] /= len(batch)
-			self.biases_gradients[i_layer] /= len(batch)
-		# subtract each partial derivative average from its corresponding parameter  # nn -= ▽C
-		for i_layer in range(1, self.n_layers):
-			self.weights[i_layer] -= self.weights_gradients[i_layer] * self.learn_step
-			self.biases[i_layer] -= self.biases_gradients[i_layer] * self.learn_step
-
-	def backpropagate(self, cost: float, target: Vector) -> tuple[list[Matrix], list[Vector]]:
+	def backpropagate(self, target: Vector) -> tuple[list[Matrix], list[Vector]]:
 		# layers partial derivatives
 		error_signals: list[Vector] = self.compute_error_signals(target)
 		# gradient
@@ -178,9 +219,10 @@ class NeuralNetwork:
 
 	def compute_gradient(self, error_signals: list[Vector]) -> tuple[list[Matrix], list[Vector]]:
 		"""
-		:return: list of gradient pairs (weights_li, biases_li)
+		:return: 2-uple of lists of gradients (weights_gradients, biases_gradients)
 		"""
 
+		# first layer has no parameters so they start with None
 		weights_gradients: list[Matrix] = [Matrix(None)]
 		biases_gradients: list[Vector] = [Vector(None)]
 
@@ -192,59 +234,16 @@ class NeuralNetwork:
 			biases_gradient_i = error_signals[i_layer]
 			biases_gradients.append(biases_gradient_i)
 
-		gradient = (weights_gradients, biases_gradients)
+		gradient: tuple[list[Matrix], list[Vector]] = (weights_gradients, biases_gradients)
 		return gradient
-
-	"""
-		analysis
-	"""
-
-	def analyse(self) -> None:
-		self.print_activations()
-		self.print_weights()
-		self.print_biases()
-
-	def print_activations(self) -> None:
-		print("\nACTIVATIONS:\n")
-		for i_layer, layer in enumerate(self.layers):
-			activations: list[str] = str(layer).split('\n')
-			for i_neuron, activation in enumerate(activations):
-				print(f"layer_{i_layer} neuron_{i_neuron}: {activation}")
-			print()
-
-	def print_weights(self) -> None:
-		print("\nWEIGHTS:\n")
-		for i_layer, matrix in enumerate(self.weights):
-			neurons_weights: list[str] = str(matrix).split('\n')
-			for i_neuron, weights in enumerate(neurons_weights):
-				print(f"layer_{i_layer} neuron_{i_neuron}: {weights}")
-			print()
-
-	def print_biases(self) -> None:
-		print("\nBIASES:\n")
-		for i_layer, layer_biases in enumerate(self.biases):
-			biases: list[str] = str(layer_biases).split('\n')
-			for i_neuron, bias in enumerate(biases):
-				print(f"layer_{i_layer} neuron_{i_neuron}: {bias}")
-			print()
 
 	"""
 		parameters backup
 	"""
 
-	""" file contents for each instance
-		
-		layers_sizes: list[int]
-		
-		wheights: list[Matrix]
-		biases: list[Vector]
-	
-	# .TXT MUST HAVE AT LEAST 1 BLANC AT THE END
-	"""
-
 	# parameters
 
-	file_path: str  # path to .txt from backups/
+	file_path: str  # path to .txt from instances_backups/
 
 	# operations
 
@@ -266,7 +265,7 @@ class NeuralNetwork:
 	def load(self) -> None:
 		with open(self.file_path, 'r') as file:
 
-			get_line(file)  # clean layers_sizes line
+			get_line(file)
 
 			for i_layer, layer_size in enumerate(self.layers_sizes[1:], 1):
 				# get matrix
@@ -279,15 +278,16 @@ class NeuralNetwork:
 		constructor
 	"""
 
-	def __init__(self, layers_sizes: list[int], backup_file_name: str, learn_step: float=1):
+	def __init__(self, layers_sizes: list[int], backup_file_name: str, data_set: tuple[tuple[list[list[float]], list[float]], tuple[list[list[float]], list[float]]]) -> None:
 		"""
 
 		"""
 
-		self.learn_step = learn_step
+		self.data_set = data_set
+		(self.x_train, self.y_train), (self.x_test, self.y_test) = self.data_set
 
 		"""backup"""
-		self.file_path = f"backups/{backup_file_name}.txt"
+		self.file_path = f"instances_backups/{backup_file_name}.txt"
 
 		"""gradient l0"""  # others are added above together with the random initial parameters
 		self.weights_gradients = [Matrix(None)]
@@ -320,7 +320,8 @@ class NeuralNetwork:
 				line: list[float] = []
 				# create new colls wich represent (a_(l1, i_line), a_(l0, i_col)) weight
 				for i_coll in range(size_l0):
-					weight: float = randint(-10, 10) / 10
+					weight: float = ((6 / (
+								self.layers_sizes[i_layer - 1] + self.layers_sizes[i_layer])) ** 0.5) * randint(-1, 1)
 					line.append(weight)
 				weights_l.append(line)
 			self.weights.append(weights_l)
@@ -338,6 +339,6 @@ class NeuralNetwork:
 
 			# fill new biases with random bias
 			for i_bias in range(layer_size):
-				bias: float = randint(-10, 10) / 500
+				bias: float = (-0.1, 0.1)[randint(0, 1)]
 				biases.append(bias)
 			self.biases.append(biases)
